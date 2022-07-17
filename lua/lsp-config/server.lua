32d45e76cc7838d2a8a8044373074a4fbd21943b
umschaudhary <umesschaudhary@gmail.com>
2022-07-17 15:45:01 +0545

    add root_file for pyright

--- lua/lsp-config/server.lua
+++ lua/lsp-config/server.lua
@@ -35,10 +35,21 @@ local lsp_flags = {
 }
 local lspconfig = require("lspconfig")
 
+local python_root_files = {
+  'WORKSPACE', -- added for Bazel; items below are from default config
+  'pyproject.toml',
+  'setup.py',
+  'setup.cfg',
+  'requirements.txt',
+  'Pipfile',
+  'pyrightconfig.json',
+}
+
 lspconfig["pyright"].setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
 	flags = lsp_flags,
+	root_dir = lspconfig.util.root_pattern(unpack(python_root_files))
 })
 lspconfig["solargraph"].setup({
 	on_attach = on_attach,
