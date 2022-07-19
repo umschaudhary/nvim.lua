32446f676bdec2df42972d66b56120fda6119d23
umschaudhary <umesschaudhary@gmail.com>
2022-07-20 01:42:42 +0545

    plugin cleanup

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,31 +1,32 @@
 return require("packer").startup(function()
 	-- Packer can manage itself
 	use("wbthomason/packer.nvim")
-	use("EdenEast/nightfox.nvim") -- Packer
+	use("EdenEast/nightfox.nvim") -- theme
 	use("kyazdani42/nvim-web-devicons") -- optional, for file icon
-	use("kyazdani42/nvim-tree.lua")
+	use("kyazdani42/nvim-tree.lua") --explorer
 
 	use("nvim-lualine/lualine.nvim") --lualine
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use("nvim-treesitter/nvim-treesitter-refactor")
-	use("windwp/nvim-ts-autotag")
-	use("windwp/nvim-autopairs")
-	use("preservim/nerdcommenter")
-	use("norcalli/nvim-colorizer.lua")
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }) --treesitter
+	use("nvim-treesitter/nvim-treesitter-refactor") --extra features on top of treesitter eg: smart rename
+	use("windwp/nvim-ts-autotag") --required by autopairs
+	use("windwp/nvim-autopairs") -- autopair plugin
+	use("preservim/nerdcommenter") -- commnter plugin
+	use("norcalli/nvim-colorizer.lua") -- show colors on live based on color codes
 	--use("Darazaki/indent-o-matic")
-	use("ggandor/lightspeed.nvim")
+	use("ggandor/lightspeed.nvim") -- search tool
 	use({
 		"lewis6991/gitsigns.nvim", -- tag = 'release' -- To use the latest release
 	})
-	use('seblj/nvim-tabline')
-	use ("jose-elias-alvarez/null-ls.nvim")
+	use('seblj/nvim-tabline') --tabs
+	use ("jose-elias-alvarez/null-ls.nvim") --plugin for formatting
 
 	use("lukas-reineke/indent-blankline.nvim") -- block lines
 	use({ "akinsho/toggleterm.nvim", tag = "v1.*" }) -- toggle term
 
-	use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } })
-	use("ur4ltz/surround.nvim")
-	use("stevearc/aerial.nvim")
+	use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } }) --project wise search
+	use("ur4ltz/surround.nvim") -- surrounding , parenthesis, brackets, quotes etc
+	use("stevearc/aerial.nvim") -- code outline plugin, handy in large files
+
 	-- LSP
 	use("neovim/nvim-lspconfig") -- Configurations for Nvim LSP
 	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
@@ -33,7 +34,7 @@ return require("packer").startup(function()
 	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
 	use("L3MON4D3/LuaSnip") -- Snippets plugin
 	use("onsails/lspkind.nvim") -- icons on completion
-	use("mhinz/vim-startify")
-	use("mattn/emmet-vim")
-	use("tpope/vim-fugitive")
+	--use("mhinz/vim-startify")
+	use("mattn/emmet-vim") --emmet html and css
+	use("tpope/vim-fugitive") --git integration
 end)
